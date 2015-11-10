6ea5f3780cc1d52bd081f81da2987dd3c9faa36c
deviantfero <unconfigured@null.spigotmc.org>
2015-11-10 11:17:33 -0600

    dividi en funciones lo de las guias

--- gtkhola.c
+++ gtkhola.c
@@ -2,49 +2,41 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+enum tipo{ FILA, COLUMNA };
+
 static void button_clicked( GtkWidget* widget, gpointer data );
-void set_label_number( gchar** string, int lim );
+void set_label_number( gchar** string, int x );
+void set_guide( GtkWidget** button, gchar** label, GtkWidget* grid, int lim, int flag );
 
 int main (int argc, char *argv[]) {
 	gtk_init (&argc, &argv);
-	gint x, y, i;
+	gint x, y;
 	printf( "INGRESE NUMERO DE COLUMNAS: " );
 	scanf( "%d", &x );
 	printf( "INGRESE NUMERO DE FILAS: " );
 	scanf( "%d", &y );
+	GtkWidget* window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
+	GtkWidget* grid = gtk_grid_new();
 	gchar** x_label = malloc( sizeof( gchar* ) * x );
 	gchar** y_label = malloc( sizeof( gchar* ) * y );
-	set_label_number( x_label, x );
-	set_label_number( y_label, y );
-	GtkWidget* window = gtk_window_new( GTK_WINDOW_TOPLEVEL );
 	GtkWidget** buttonx = malloc( sizeof( GtkWidget* ) * x );
 	GtkWidget** buttony = malloc( sizeof( GtkWidget* ) * y );
-	GtkWidget* label = gtk_label_new( "Hello world" );
-	GtkWidget* grid = gtk_grid_new();
 	//con estos dos for establezco una fila y una columna de botones
 	//cuyo rango puede variar de 1 - 99
-	for( i = 0; i < x; i++ ){
-		buttonx[i] = gtk_button_new_with_label( x_label[i] );
-		gtk_grid_attach( (GtkGrid*)grid, buttonx[i], i + 1, 1, 1, 1 );
-	}
-	for( i = 0; i < y; i++ ){
-		buttony[i] = gtk_button_new_with_label( y_label[i] );
-		gtk_grid_attach( (GtkGrid*)grid, buttony[i], 1, i + 1, 1, 1 );
-	}
+	set_guide( buttonx, x_label, grid, x, COLUMNA );
+	set_guide( buttony, y_label, grid, y, FILA );
 	g_signal_connect( window, "delete_event",G_CALLBACK(gtk_main_quit), NULL );
-	g_signal_connect( buttonx[0], "clicked", G_CALLBACK( button_clicked ), (gpointer)label );
 	//evento de click, uso un puntero a label para modificarlo en la funcion definida
 	//arriba, cuando uso G_CALLBACK se llama la funcion con los argumentos button y
-	//(gpointer)label
-	gtk_grid_attach( (GtkGrid*)grid, label, 3, 2, 1, 1 );
+	//(gpointer)algo, con lo cual se interactua
 	gtk_grid_set_row_spacing( (GtkGrid*)grid, 2 );
 	gtk_grid_set_column_spacing( (GtkGrid*)grid, 2 );
-	//estas dos lineas lo hacen adaptable a la pantalla, las celdas
-	//siempre tendran el mismo tamaño entre si
 	gtk_grid_set_column_homogeneous( (GtkGrid*)grid, TRUE );
 	gtk_grid_set_row_homogeneous( (GtkGrid*)grid, TRUE );
+	//estas dos lineas lo hacen adaptable a la pantalla, las celdas
+	//siempre tendran el mismo tamaño entre si
 	gtk_container_add( (GtkContainer*)window, grid );
-	gtk_window_set_title( (GtkWindow*)window, "Senpai simulator 2015" );
+	gtk_window_set_title( (GtkWindow*)window, "Hoja de Calculo - Calctech" );
 	gtk_widget_show_all( window );
 	gtk_main();
     return 0;
@@ -55,7 +47,7 @@ static void button_clicked( GtkWidget* widget, gpointer data ){
 }
 
 void set_label_number( gchar** string, int lim ){
-	int i = 0, j = 0, vuelta = 0;
+	gint i = 0, j = 0, vuelta = 0;
 	if( lim > 99 )
 		return;
 	for( i = 0; i < lim; i++ ){
@@ -73,6 +65,22 @@ void set_label_number( gchar** string, int lim ){
 				j = 0;
 				vuelta++;
 			}
+		}
+	}
+}
+
+void set_guide( GtkWidget** button, gchar** label, GtkWidget* grid, int lim, int flag ){
+	gint i;
+	set_label_number( label, lim );
+	if( flag == COLUMNA ){
+		for( i = 0; i < lim; i++ ){
+			button[i] = gtk_button_new_with_label( label[i] );
+			gtk_grid_attach( (GtkGrid*)grid, button[i], i + 1, 1, 1, 1 );
+		}
+	}else{
+		for( i = 0; i < lim; i++ ){
+			button[i] = gtk_button_new_with_label( label[i] );
+			gtk_grid_attach( (GtkGrid*)grid, button[i], 1, 1 + i, 1, 1 );
 		}
 	}
 }
